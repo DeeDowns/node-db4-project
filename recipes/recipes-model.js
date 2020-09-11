d29082380ba75865e65b8a120793a49b646da5bc
DeeDowns <dee_downs@outlook.com>
2020-09-10 21:34:14 -0400

    getRecipes and getShoppingList helper functions complete

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -1,9 +1,19 @@
 const db = require('../data/db-config')
 
 module.exports = {
-    getRecipes
+    getRecipes,
+    getShoppingList,
+    // getInstructions
 }
 
 function getRecipes() {
     return db('recipes')
+}
+
+function getShoppingList(recipe_id) {
+    return db('recipe_ingredients')
+    .join('ingredients', 'ingredients.id', 'recipe_ingredients.ingredient_id')
+    .select('ingredients.ingredient_name','recipe_ingredients.quantity', 'recipe_ingredients.unit_of_measurement' )
+    .where('recipe_ingredients.recipe_id', recipe_id)
+
 }
--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -17,5 +17,18 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id/shopping-list', (req, res) => {
+    const { id } = req.params
+    Recipes.getShoppingList(id)
+    .then(list => {
+        console.log(list)
+        res.status(200).json({ data: list })
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    })
+})
+
 
 module.exports = router
